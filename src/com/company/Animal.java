69fbaf2a0f92fe9ecf910915fca2d6acbca696a0
fixerin <jan.malinski94@gmail.com>
2021-02-28 13:47:02 +0100

    Comparing weight of two animals.

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,12 +1,14 @@
 package com.company;
 
 public class Animal {
+    String name;
     int weight;
     int age;
     String species;
 
 
-    public Animal(int weight, int age, String species) {
+    public Animal(String name, int weight, int age, String species) {
+        this.name = name;
         this.weight = weight;
         this.age = age;
         this.species = species;
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,8 +9,8 @@ public class Main {
         Human human1 = new Human( "Jan", "Malinski", "Java Dev", 6000);
         Human human2 = new Human( "Wojtek", "Krawczyk", "Front-End Dev", 5500 );
 
-        Animal animal1 = new Animal( 15, 3, "cat" );
-        Animal animal2 = new Animal( 40, 8, "dog" );
+        Animal animal1 = new Animal( "Gutek", 15, 3, "cat" );
+        Animal animal2 = new Animal("Loui",  40, 8, "dog" );
 
         Phone phone1 = new Phone( "Motorola", 0.25, 1500.0 );
         Phone phone2 = new Phone( "Samsung", 0.3, 2214.23 );
@@ -18,6 +18,15 @@ public class Main {
         System.out.println(human1);
         System.out.println(phone2);
         System.out.println(animal1);
+
+        if (animal1.weight > animal2.weight){
+            System.out.println(animal1.name + " jest ciezszy od " + animal2.name + ".");
+            System.out.println("Waga " + animal1.name + " to " + animal1.weight + "kg.");
+        } else {
+            System.out.println(animal2.name + " jest ciezszy od " + animal1.name + ".");
+            System.out.println("Waga " + animal2.name + " to " + animal2.weight + "kg.");
+
+        }
 
     }
 }
